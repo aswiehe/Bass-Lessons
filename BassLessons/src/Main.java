fbd3df4bfca8d6355fde00e3375c729e6c8547c9
aswiehe <averywiehe@gmail>
2017-06-01 19:42:29 -0400

    Created images of transposed notes in MS Paint. They've been included in the switch statement, but a group will need to be created so they can be added in getChildren. Probably will have to come up with a way to eliminate the redundancy and correct to naming conventions.

--- BassLessons/src/Main.java
+++ BassLessons/src/Main.java
@@ -4,16 +4,32 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class Main extends Application {
 	public void start(Stage stage){
+		
+		//setting stage and scene
 		Group group = new Group();
 		Scene scene = new Scene(group, 1200, 800);
 		stage.setTitle("Bass Lessons");		
 		stage.setScene(scene);
 		stage.show();
 
+		//passing in bass clef image
+		//Image bassClef = new Image("Large_Bass_Clef.png");
+		//ImageView iv = new ImageView();
+		//iv.setImage(bassClef);
+		
+		//
+		
+		//passing in random notes
+		new RandNote().randNotes();
+		
+		
 		//added two buttons
 		Button transcription = new Button("Transcription");
 		Button transposition = new Button("Transposition");		
@@ -24,13 +40,13 @@ public class Main extends Application {
 		transposition.setLayoutY(20);
 		transposition.setLayoutX(120);
 		//added two labels
-		Label transcribing = new Label("Transcribing...");
-		Label transposing = new Label("Transposing...");
+		Label transcribing = new Label("Transcribing... ");
+		Label transposing = new Label("Transposing... ");
 		//set transcribing and transposing labels on specific location
-		transcribing.setLayoutY(100);
-		transcribing.setLayoutX(50);
-		transposing.setLayoutY(100);
-		transposing.setLayoutX(50);
+		transcribing.setLayoutY(50);
+		transcribing.setLayoutX(75);
+		transposing.setLayoutY(50);
+		transposing.setLayoutX(75);
 		//set labels to default invisible
 		transcribing.setVisible(false);
 		transposing.setVisible(false);
@@ -42,27 +58,14 @@ public class Main extends Application {
 		transposition.setOnAction(e -> {
 			transcribing.setVisible(false);
 			transposing.setVisible(true);
+			//char newnote = new RandNote().randNotes();
 		});
 		//used the addAll method to condense added group node for scene
 		group.getChildren().addAll(transcription, transcribing, transposition, transposing);
 		
 		//to be implemented with random integer generator to generate assigned random notes
 		//lower case letters indicate flats, naturals are indicated with upper case letters
-		int C = 0;
-		int d = 1;
-		int D = 2;
-		int e = 3;
-		int E = 4;
-		int F = 5;
-		int g = 6;
-		int G = 7;
-		int a = 8;
-		int A = 9;
-		int b = 10;
-		int B = 11;
-		int c = 12;		
 	}
-	
 	public static void main(String[] args){
 		Application.launch();
 	}
